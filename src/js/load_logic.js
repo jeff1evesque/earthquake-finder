2f32fc7e2e9bcc901cde3bd54b6b5b974d3026ea
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-20 16:30:47 -0500

    #15: load_logic.js, add dom-load wrapper

--- src/js/load_logic.js
+++ src/js/load_logic.js
@@ -2,3 +2,7 @@
 * load_logic.js: stores HTML form data into jQuery variables, then determine strongest
 *                earthquake within provided 'radius', and 'timeframe', respectively.
 */
+
+$(function() {
+
+});
