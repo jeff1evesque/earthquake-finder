00bcbe5f9694488b3139f2d206bbf33d1dabfd0c
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-20 16:28:15 -0500

    #15: load_logic.js, add file with docblock

---
+++ src/js/load_logic.js
@@ -0,0 +1,4 @@
+/**
+* load_logic.js: stores HTML form data into jQuery variables, then determines strongest
+*                earthquake within provided 'radius', and 'timeframe' values, respectively.
+*/
