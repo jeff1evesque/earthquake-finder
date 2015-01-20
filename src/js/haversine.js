74fa1ed646de7a35026833047da4bedf4e1b321f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-20 14:56:47 -0500

    #7: haversine.js, change 'R' -> 'radius'

--- src/js/haversine.js
+++ src/js/haversine.js
@@ -14,13 +14,13 @@
  * getDistance: computes distance between two GPS coordinate points.
  */
   var getDistance = function(p1, p2) {
-    var R = 6378137; // Earth’s mean radius in meter
+    var radius = 6378137; // Earth’s mean radius in meter
     var dLat = rad(p2.lat() - p1.lat());
     var dLong = rad(p2.lng() - p1.lng());
     var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.cos(rad(p1.lat())) * Math.cos(rad(p2.lat())) *
       Math.sin(dLong / 2) * Math.sin(dLong / 2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c;
+    var d = radius * c;
     return d; // returns the distance in meter
   };
