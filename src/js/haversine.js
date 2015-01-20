d09be9a3f187f89e6dd2e72f82f55b2dbc65245a
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-20 14:52:57 -0500

    #7: haversine.js, indent 'getDistance' function

--- src/js/haversine.js
+++ src/js/haversine.js
@@ -9,3 +9,15 @@
   var rad = function(x) {
     return x * Math.PI / 180;
   };
+
+  var getDistance = function(p1, p2) {
+    var R = 6378137; // Earth’s mean radius in meter
+    var dLat = rad(p2.lat() - p1.lat());
+    var dLong = rad(p2.lng() - p1.lng());
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(rad(p1.lat())) * Math.cos(rad(p2.lat())) *
+      Math.sin(dLong / 2) * Math.sin(dLong / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d; // returns the distance in meter
+  };
