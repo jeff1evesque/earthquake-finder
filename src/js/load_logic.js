81a5b6e63683c7642fb93c4a92deb0eeddd77271
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-20 16:29:01 -0500

    #15: load_logic.js, minor language change

--- src/js/load_logic.js
+++ src/js/load_logic.js
@@ -1,4 +1,4 @@
 /**
-* load_logic.js: stores HTML form data into jQuery variables, then determines strongest
-*                earthquake within provided 'radius', and 'timeframe' values, respectively.
+* load_logic.js: stores HTML form data into jQuery variables, then determine strongest
+*                earthquake within provided 'radius', and 'timeframe', respectively.
 */
