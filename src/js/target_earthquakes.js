72f8f06f8e6d97ef1950bea0c44094eea41f7d0f
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-20 17:36:13 -0500

    #19: target_earthquakes.js, define file docblock

--- src/js/target_earthquakes.js
+++ src/js/target_earthquakes.js
@@ -1,5 +1,16 @@
 /**
 * target_earthquakes.js: iterate over supplied geojson file, then return array
-*                        of earthquakes within supplied restriction 'gps_radius',
-*                        and 'timeframe'.
+*                        of earthquakes within supplied restriction:
+*
+* @origin_longitude, longitude value of the origin point.
+* @origin_latitude, latitude value of the origin point.
+* @radius, acceptable distance (mi) from the origin point (longitude, latitude).
+* @negDays, acceptable date range [-negDays, today] for return values.
 */
+
+/**
+ * target_asset: converts degrees to radians.
+ */
+  var target_asset = function(origin_longitude, origin_latitude, radius, timeframe) {
+  };
+
