1355384ac613adb91b3b5a027f8ec9c8b77cabc6
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-20 14:49:41 -0500

    #7: haversine.js, function to convert degrees to radians

--- src/js/haversine.js
+++ src/js/haversine.js
@@ -2,3 +2,7 @@
 * haversine.js: contains necessary functions needed to compute the distance between
 *               two GPS coordinate points (longitude, latitude).
 */
+
+var rad = function(x) {
+  return x * Math.PI / 180;
+};
