671e0d19a61357c3ea61e19bab3672510de38306
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-20 17:52:46 -0500

    #19: target_earthquakes.js, restructure docblock, and function name

--- src/js/target_earthquakes.js
+++ src/js/target_earthquakes.js
@@ -1,16 +1,17 @@
 /**
-* target_earthquakes.js: iterate over supplied geojson file, then return array
-*                        of earthquakes within supplied restriction:
-*
-* @origin_longitude, longitude value of the origin point.
-* @origin_latitude, latitude value of the origin point.
-* @radius, acceptable distance (mi) from the origin point (longitude, latitude).
-* @negDays, acceptable date range [-negDays, today] for return values.
+* target_earthquakes.js: contains global function(s) needed to determine an array
+*                        of earthquakes within supplied restriction.
 */
 
 /**
- * target_asset: converts degrees to radians.
+ * return_earthquakes: return an array of earthquakes within 'negdays', and 'radius'
+ *                     of supplied origin.
+ *
+ * @origin_longitude, longitude value of the origin point.
+ * @origin_latitude, latitude value of the origin point.
+ * @radius, acceptable distance (mi) from the origin point (longitude, latitude).
+ * @negDays, acceptable date range [-negDays, today] for return values.
  */
-  var target_asset = function(origin_longitude, origin_latitude, radius, negDays) {
+  var return_earthquakes = function(origin_longitude, origin_latitude, radius, negDays) {
   };
 
