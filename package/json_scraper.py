82000f5873ea53040ccf2ae52c729be02b8a20ea
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-21 18:03:25 -0500

    #59: json_scraper.py, fix several typos

--- package/json_scraper.py
+++ package/json_scraper.py
@@ -6,12 +6,11 @@
 import sys, urllib2, json
 
 ## scraper: scrape the content of provided url.
-def scraper():
-  if len( sys.argv ) > 0:
-    # open given url, store the corresponding json response
-    response = urllib2.urlopen(sys.argv[1])
-    data     = json.load(response)
-    response.close()
+def scraper(url):
+  # open given url, store the corresponding json response
+  response = urllib2.urlopen(url)
+  data     = json.loads(response)
+  response.close()
 
-    # return content
-    print data
+  # return content
+  return data
