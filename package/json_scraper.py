91248528cb784ae150c7e7f01788312a37452661
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-21 18:04:23 -0500

    #59: json_scraper.py, fix several typos

--- package/json_scraper.py
+++ package/json_scraper.py
@@ -3,10 +3,10 @@
 ## @json_scraper.py
 #  This file scrapes the content of an external webpage, and returns the
 #      parsed json response to javascript (retriever_dataset.js) via AJAX.
-import sys, urllib2, json
+import sys, urllib2
 
-## scraper: scrape the content of provided url.
-def scraper(url):
+## scrape: scrape the content of provided url.
+def scrape(url):
   # open given url, store the corresponding json response
   response = urllib2.urlopen(url)
   data     = json.loads(response)
