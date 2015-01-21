398b3e7081d2a75baf02ae69f6a3a29fb61f046b
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-21 12:13:28 -0500

    #40: app.py, add comments

--- app.py
+++ app.py
@@ -3,12 +3,15 @@
 #    allows the presentation of (asynchronous) content.
 from flask import Flask, render_template, request, jsonify
 
+# Initialize: create flask instance
 app = Flask(__name__)
 
+# Define Route: assign corresponding template, or logic to given path
 @app.route('/')
 def index():
   return render_template('index.html')
 
+# Execute: run application directly, instead of import
 if __name__ == '__main__':
   app.run(
     debug=True
