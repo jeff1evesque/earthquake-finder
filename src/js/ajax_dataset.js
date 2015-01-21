9ca7cd9e9f104c98464d82b6cb0029d97f7a84c9
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-20 20:21:42 -0500

    #21: ajax_dataset.js, skeleton function with ajax call

--- src/js/ajax_dataset.js
+++ src/js/ajax_dataset.js
@@ -6,5 +6,16 @@
  * dataset_earthquake: parse dataset from given url.
  */
   var dataset_earthquake = function(dataset_url) {
+    $.ajax({
+      url: dataset_url,
+      type: 'POST',
+      beforeSend: function() {
 
+      }
+    }).done(function(data) {
+
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+      console.log('Error Thrown: '+errorThrown);
+      console.log('Error Status: '+textStatus);
+    });
   }
