03f025177da7d43ed5d3e9fd1f7c7e03f616ed18
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-21 13:02:11 -0500

    #49: web_scraper.py, retrieve designated webpage, and save content

--- python/web_scraper.py
+++ python/web_scraper.py
@@ -4,3 +4,7 @@
 #  This file scrapes the content of an external webpage.
 from lxml import html
 import requests, sys
+
+if len( sys.argv ) > 0:
+  page = requests.get(sys.argv[1])
+  tree = html.fromstring(page.text)
