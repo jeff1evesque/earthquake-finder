a26471604b7a2e453431cadd22a448e801bf1e43
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-21 13:23:14 -0500

    #49: web_scraper.py, adjust docblock, add comment

--- python/web_scraper.py
+++ python/web_scraper.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
 ## @web_scraper.py
-#  This file scrapes the content of an external webpage.
+#  This file scrapes the content of an external webpage, and returns the
+#      parsed response to javascript (retriever_dataset.js) via AJAX.
 import sys, urllib2, json
 
 if len( sys.argv ) > 0:
@@ -9,4 +10,5 @@ if len( sys.argv ) > 0:
   response = urllib2.urlopen(sys.argv[1])
   data     = json.load(response)
 
+  # return content
   print data
