c152502f8473b6ef3a74c8af8d412e9f17d55e4e
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-20 20:16:37 -0500

    #21: ajax_dataset.js, add docblock for 'dataset_earthquake'

--- src/js/ajax_dataset.js
+++ src/js/ajax_dataset.js
@@ -2,7 +2,9 @@
 * ajax_dataset.js: return parsed dataset from given URL.
 */
 
-
+/**
+ * dataset_earthquake: parse dataset from given url.
+ */
   var dataset_earthquake = function(dataset_url) {
 
   }
