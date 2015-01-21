49bd89f75db478f32ab05dd3fc925723aa981e79
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-21 12:06:42 -0500

    #40: app.py, add file docblock

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+## @app.py
+#  This file loads corresponding logic, and html template file(s), which
+#    allows the presentation of (asynchronous) content.
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
