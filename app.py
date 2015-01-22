8ed0876b167601b1ef07e481c69d3381daccaa2b
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-22 10:39:58 -0500

    #86: app.py, implement 'Data_Iterator'

--- app.py
+++ app.py
@@ -16,7 +16,10 @@ def index():
 @app.route('/json_scraper/', methods=['POST', 'GET'])
 def json_scraper():
   if request.method == 'POST':
-    return scrape(request.form['gps_dataset'])
+    dataset = scrape(request.form['gps_dataset'])
+
+    target  = Data_Iterator( dataset, request.form['gps_longitude'], request.form['gps_latitude'], request.form['gps_radius'], request.form['daysBack'] )
+    target.iterator()
 
 # Execute: run application directly, instead of import
 if __name__ == '__main__':
