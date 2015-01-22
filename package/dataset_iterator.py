95d2e7ebfa366052a21fc68031a9acafd312bdec
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-22 11:05:04 -0500

    #75: dataset_iterator.py, fix typo and indentation

--- package/dataset_iterator.py
+++ package/dataset_iterator.py
@@ -5,16 +5,16 @@
 #      within the supplied parameters (i.e. radius, timeframe).
 
 ## Class: Data_Iterator
-class Data_Iterator
+class Data_Iterator:
 
   ## constructor:
   def __init__(self, dataset, longitude, latitude, radius, daysBack):
-  self.target   = []
-  self.dataset  = dataset
-  self.origin_longitude = longitude
-  self.origin_latitude  = latitude
-  self.radius   = radius
-  self.daysBack = daysBack
+    self.target   = []
+    self.dataset  = dataset
+    self.origin_longitude = longitude
+    self.origin_latitude  = latitude
+    self.radius   = radius
+    self.daysBack = daysBack
 
   ## iterator: iterate a given dataset, and store earthquakes within the specified radius,
   #            and timeframe.
