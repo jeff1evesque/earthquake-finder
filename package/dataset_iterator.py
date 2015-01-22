dc9cddad19f670ae6d0073bb69573619174da4a3
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-22 10:26:29 -0500

    #75: dataset_iterator.py, define class variables

--- package/dataset_iterator.py
+++ package/dataset_iterator.py
@@ -8,9 +8,13 @@
 class Data_Iterator
 
   ## constructor:
-  def __init__(self, dataset):
-  self.dataset = dataset
-  self.target  = []
+  def __init__(self, dataset, longitude, latitude, radius, daysBack):
+  self.target   = []
+  self.dataset  = dataset
+  self.origin_longitude = longitude
+  self.origin_latitude  = latitude
+  self.radius   = radius
+  self.daysBack = daysBack
 
   ## iterator: iterate a given dataset, and store earthquakes within the specified radius,
   #            and timeframe.
