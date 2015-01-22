a1c1045b971bc793fff4a9e17807edfa2a211701
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-22 10:14:57 -0500

    #75: dataset_iterator.py, outline logic

--- package/dataset_iterator.py
+++ package/dataset_iterator.py
@@ -3,3 +3,18 @@
 ## @dataset_iterator.py
 #  This file iterates a given dataset, and returns a list of dict records, of earthquakes
 #      within the supplied parameters (i.e. radius, timeframe).
+
+## Class: Data_Iterator
+class Data_Iterator
+
+  ## constructor:
+  def __init__(self, dataset):
+  self.dataset = dataset
+  self.target  = []
+
+  ## iterator: iterate given dataset, and store earthquakes within the specified radius,
+  #            and timeframe.
+  def iterator(self):
+
+  ## get_target: returns a list of earthquakes within the supplied parameters.
+  def get_target(self):
