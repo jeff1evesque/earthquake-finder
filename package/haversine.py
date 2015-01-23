7384ea750ed4af3b28909c803b6cd4d154357195
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-23 12:07:45 -0500

    #75: haversine.py, define 'degrees_to_radians'

--- package/haversine.py
+++ package/haversine.py
@@ -3,3 +3,8 @@
 ## @haversine.py
 #  This file contains the necesary methods needed to compute the distance between
 #      two GPS coordinate points (longitude, latitude).
+import math
+
+## degrees_to_radians: converts supplied value from degrees, to radians.
+def degrees_to_radians(deg):
+  return deg * (math.pi / 180)
