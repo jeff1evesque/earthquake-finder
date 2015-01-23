d847e1009413b356b808bbf7d6887f2ec1543c21
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-23 12:32:41 -0500

    #74: haversine.py, remove 'degrees_to_radians', define 'get_distance'

--- package/haversine.py
+++ package/haversine.py
@@ -3,8 +3,23 @@
 ## @haversine.py
 #  This file contains the necesary methods needed to compute the distance between
 #      two GPS coordinate points (longitude, latitude).
-import math
+from math import radians, cos, sin, asin, sqrt
 
-## degrees_to_radians: converts supplied value from degrees, to radians.
-def degrees_to_radians(deg):
-  return deg * (math.pi / 180)
+## get_distance: computes the distance between two GPS coordinate points using the
+#                haversine formula
+def get_distance(p1, p2):
+  # radius of the earth
+  radius = 6378137
+
+  # convert decimal degrees to radians
+  lon1, lat1, lon2, lat2 = map(radians, [p1['longitude'], p1['latitude'], p2['longitude'], p2['latitude']])
+
+  # haversine formula
+  dlon = lon2 - lon1
+  dlat = lat2 - lat1
+  a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+  c = 2 * asin(sqrt(a))
+
+  # return distance
+  d = radius * c
+  return d
