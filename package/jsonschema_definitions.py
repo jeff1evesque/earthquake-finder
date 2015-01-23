17c186448299d0955a51b24828205ef36b13ae78
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-23 10:32:55 -0500

    #81: jsonschema_definition.py, define 'jsonschema_data'

--- package/jsonschema_definitions.py
+++ package/jsonschema_definitions.py
@@ -2,3 +2,37 @@
 
 ## @jsonschema_definitions.py
 #  This file contains various jsonschema definitions.
+
+## jsonschema_data(): contains the jsonschema for each iterated element of the
+#                     geojson dataset.
+def jsonschema_data():
+  schema = {
+    'type': 'object',
+    'properties': {
+      'id': {
+        'type': 'string',
+        'minLength': 1
+      },
+      'coordinates': {
+        'type': 'array',
+         'items': {
+           'type': 'number',
+           'minLength': 1,
+         },
+         'minItems': 1
+      },
+      'magnitude': {
+        'type': 'number',
+        'minLength': 1
+      },
+      'time': {
+        'type': 'integer',
+        'minLength': 1
+      },
+      'location': {
+        'type': 'string',
+        'minLength': 1
+      }
+    }
+  }
+  return schema
