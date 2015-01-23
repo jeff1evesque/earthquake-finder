f19c9e71991cb664b277b33ca121ff51cea75596
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-23 16:08:25 -0500

    #78: form_validator.js, add file and docblock

---
+++ static/js/form_validator.js
@@ -0,0 +1,8 @@
+/**
+ * @form_validator.js: call 'validate()' method on defined form elements.
+ *
+ *     http://jqueryvalidation.org/documentation/
+ *     http://jqueryvalidation.org/category/validator/
+ *     http://jqueryvalidation.org/jQuery.validator.addMethod/
+ *     http://stackoverflow.com/questions/10843399#answer-10843593
+ */
