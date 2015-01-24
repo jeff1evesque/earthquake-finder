e2351df7ad141b67c68faac5208eca3d198c4a36
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-23 20:13:47 -0500

    #79: ajax_data.js, implement 'submitHandler'

--- static/js/ajax_data.js
+++ static/js/ajax_data.js
@@ -5,38 +5,35 @@
  */
 
 $(function() {
-  $('form').on('submit', function(event) {
-    event.preventDefault();
+  $('form').validate({
+    submitHandler: function() {
 
-  // Local Variables
-    var form_data = $('form').serialize();
+    // Local Variables
+      var form_data = $('form').serialize();
 
-  // Compute Largest Earthquake
-    $.ajax({
-      type: 'POST',
-      url: '/json_scraper/',
-      data: form_data,
-      beforeSend: function() {
-        $("form").validate({
-          submitHandler: function(form) {
-            $(form).ajaxSubmit();
-          }
-        });
+    // Compute Largest Earthquake
+      $.ajax({
+        type: 'POST',
+        url: '/json_scraper/',
+        data: form_data,
+        beforeSend: function() {
+          var spinner = new ajaxLoader( $(event.currentTarget) );
+        }
+      }).done(function(data) {
+      // Remove AJAX Overlay
+        $('form .ajax_overlay').fadeOut(200, function(){ $(this).remove() });
+      // Append Results
+        $('.result').remove();
+        $('.fieldset_parameters').after( '<p class="result">' + data + '</p>' );
+      }).fail(function(jqXHR, textStatus, errorThrown) {
+        $('.result').remove();
+        $('.fieldset_parameters').after( '<p class="result error">Error: Could not submit request. Please review the messages in the browser \'console.log\'</p>' );
+        console.log('Error Thrown: '+errorThrown);
+        console.log('Error Status: '+textStatus);
+      });
 
-        var spinner = new ajaxLoader( $(event.currentTarget) );
-      }
-    }).done(function(data) {
-    // Remove AJAX Overlay
-      $('form .ajax_overlay').fadeOut(200, function(){ $(this).remove() });
-    // Append Results
-      $('.result').remove();
-      $('.fieldset_parameters').after( '<p class="result">' + data + '</p>' );
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-      $('.result').remove();
-      $('.fieldset_parameters').after( '<p class="result error">Error: Could not submit request. Please review the messages in the browser \'console.log\'</p>' );
-      console.log('Error Thrown: '+errorThrown);
-      console.log('Error Status: '+textStatus);
-    });
-
+    // prevent form redirect
+      return false;
+    }
   });
 });
