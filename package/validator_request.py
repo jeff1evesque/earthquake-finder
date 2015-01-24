7526d08bcb46cff974170a5bab33520240794fbe
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-24 08:41:26 -0500

    #139: validator_request.py, remove validation logic

--- package/validator_request.py
+++ package/validator_request.py
@@ -9,50 +9,6 @@ from package.validator_functions import validate_longitude, validate_latitude, v
 def validate_request( origin ):
   list_error = []
 
-  # Validate Longitude
-  try:
-    # check float
-    float(origin['longitude'])
-
-    # check bounds
-    if -180 <= origin['longitude'] <= 180: pass
-    else: self.list_error.append('longitude value: ' . + str(origin['longitude']) + ', must be between [-180, 180]')
-  except ValueError, e:
-    self.list_error.append('longitude must be an integer, or decimal value.')
-
-  # Validate Latitude
-  try:
-    # check float
-    float(origin['latitude'])
-
-    # check bounds
-    if -90 <= origin['latitude'] <= 90: pass
-    else: self.list_error.append('latitude value: ' . + str(origin['latitude']) + ', must be between [-90, 90]')
-  except ValueError, e:
-    self.list_error.append('latitude must be an integer, or decimal value.')
-
-  # Validate Radius
-  try:
-    # check float
-    float(origin['radius'])
-
-    # check bounds
-    if origin['radius'] >= 0: pass
-    else: self.list_error.append('radius value: ' . + str(origin['radius']) + ', must be greater than, or equal to 0')
-  except ValueError, e:
-    self.list_error.append('radius must be an integer, or decimal value.')
-
-  # Validate DaysBack
-  try:
-    # check int
-    int(origin['daysBack'])
-
-    # check bounds
-    if origin['daysBack'] >= 0: pass
-    else: self.list_error.append('daysBack value: ' . + str(origin['daysBack']) + ', must be greater than, or equal to 0')
-  except ValueError, e:
-    self.list_error.append('radius must be an integer value.')
-
   # return error
   if len(list_error) > 0:
     return { 'status': False, 'error': list_error }
