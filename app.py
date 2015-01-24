6988cc80bf6b70de229ecbce7ff8e3e9e2eb2871
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-23 23:55:49 -0500

    #123: app.py, 'import validate'

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template, request
 from package.json_scraper import scrape
 from package.dataset_iterator import Data_Iterator
+from jsonschema import validate
 from package.jsonschema_definitions import jsonschema_request
 import json
 
