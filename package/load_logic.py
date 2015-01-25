43e44d0bcc9ea04be0700e96c9bfff9c00fba8f3
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-25 16:03:08 -0500

    #207: load_logic.py, create file with docblock

---
+++ package/load_logic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+## @load_logic.py
+#  This file contains the necessary logic needed to determine the largest magnitude
+#      earthquake from the given dataset, relative to the supplied GPS coordinates,
+#      with respect to the acceptable radius, and number of days back from today
+#      (when webform, or command is submitted).
+#
+#  Specifically, this file can be run via the command line, or via the supplied
+#      web-interface (app.py). Form more information, refer to the README.md.
