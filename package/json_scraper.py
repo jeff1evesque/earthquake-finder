e938e2003d95e1274fbd292832a9f54893825a48
Jeff Levesque <jeff1evesque@yahoo.com>
2015-01-25 13:21:49 -0500

    #59: json_scraper.py, change comment

--- package/json_scraper.py
+++ package/json_scraper.py
@@ -7,7 +7,7 @@ import requests
 
 ## scrape: scrape the content of provided url.
 def scrape(url):
-  # open given url, store the corresponding json response
+  # request (get) given url, store json response content
   r = requests.get(url)
   data = r.json()
 
