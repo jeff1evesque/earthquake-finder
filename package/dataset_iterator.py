1afc582bb0262545d248b9db58608029ff877725
Jeff Levesque <jeff1evesque@yahoo.com>
2015-03-29 10:45:56 -0400

    #232: dataset_iterator.py, 'import Draft4Validator'

--- package/dataset_iterator.py
+++ package/dataset_iterator.py
@@ -4,7 +4,7 @@
 #  This file iterates a given dataset, and returns a list of dict records, of earthquakes
 #      within the supplied parameters (i.e. radius, timeframe).
 import json, time
-from jsonschema import validate
+from jsonschema.validators import Draft4Validator
 from package.jsonschema_definitions import jsonschema_data
 from package.haversine import get_distance
 
